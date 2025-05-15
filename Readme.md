dadaf785a62e792336883b3710b6e25b2e7eceee
William Gray <wgray4784@sdsu.edu>
2025-05-15 10:27:28 -0700

    Completed Programming Assignment 3

--- Readme.md
+++ Readme.md
@@ -2,10 +2,10 @@
 # Prim's Algorithm with Min Heap and Adjacency Matrix
 
 ## Author
-Your Name Here
+William Gray
 
 ## Description
-Briefly describe what your program does and how the code is structured.
+This program is an implementation of Prim's algorithm to find the minimum spanning tree (MST) of a given connected, undirected, and weighted graph. It uses an adjacency matrix to store the edge weights between vertices, and it uses a min heap to store the vertices and edge weights. The min heap is built from a priority queue, which makes sure that the next vertex requiring the lowest cost (edge weight) is next in line to be added to the MST. This ensures that the min heap will help get the correct vertex that can be added to the MST.
 
 ## Files
 - graph.h / graph.cpp
@@ -20,15 +20,38 @@ Run directly from the IDE.
 ## Time Complexity Analysis
 
 
-| Operation            | Time Complexity   |
-|----------------------|-------------------|
-| Insert in MinHeap    |                   |
-| Extract Min          |                   |
-| Decrease Key         |                   |
-| Prim’s MST Overall   |                   |
+| Operation            | Time Complexity |
+|----------------------|-----------------|
+| Insert in MinHeap    | O(log n)        |
+| Extract Min          | O(log n)        |
+| Decrease Key         | O(log n)        |
+| Prim’s MST Overall   | O(n^2)          |
 
 _Explain why your MST implementation has the above runtime._
+* Insert in MinHeap - The time complexity for inserting into the min heap is O(log n), where n is the number of elements in the heap. After the new vertex is added, it must be properly placed into the min heap. Since the while loop splits the possible locations for the vertex to go in half each time through, it runs at O(log n) time.
+* Extract Min - The time complexity for extracting the minimum element in the min heap is O(log n). After the minimum element is extracted, the min heap must be fixed to restore the min heap property (parent must be smaller than child). To do this, minHeapify(int idx) is called, which runs at O(log n) time because it recursively fixes the min heap, making the problem smaller and smaller each time through.
+* Decrease Key - The time complexity for the function decreaseKey(int vertex, int newKey) is O(log n). This function is very similar to the function for inserting into the min heap since it contains the same while loop that is used to fix the min heap after updating the key of the vertex. This while loop splits the problem in half each time, resulting in O(log n) time.
+* Prim's MST Overall - The time complexity for Prim's MST is O(n^2), where n is the number of vertices. Since the algorithm checks all other vertices to find the lowest edge weight for each vertex, it runs at O(n^2) time (n vertices * n checks per vertex = n^2). 
 
 ## Test Case Description
 
 Input:  
+Graph g(6);  
+g.addEdge(0, 1, 4);  
+g.addEdge(0, 2, 3);  
+g.addEdge(0, 5, 2);  
+g.addEdge(1, 2, 1);  
+g.addEdge(1, 3, 2);  
+g.addEdge(1, 4, 4);  
+g.addEdge(2, 3, 4);  
+g.addEdge(3, 4, 2);  
+g.addEdge(4, 5, 5);  
+g.primMST();  
+  
+Output:  
+2--1 (1)  
+0--2 (3)  
+1--3 (2)  
+3--4 (2)  
+0--5 (2)  
+Total cost: 10
--- heap.h
+++ heap.h
@@ -1,15 +1,82 @@
 #ifndef HEAP_H
 #define HEAP_H
+using namespace std;
 
 class MinHeap {
 public:
-    MinHeap(int capacity);
-    ~MinHeap();
-    void insert(int vertex, int key);
-    int extractMin();
-    void decreaseKey(int vertex, int newKey);
-    bool isInMinHeap(int vertex);
-    bool isEmpty();
+    MinHeap(int capacity) { //Constructor
+        this->capacity = capacity;
+        size = 0;
+        heapArray = new int[capacity];
+        keyArray = new int[capacity];
+        position = new int[capacity];
+
+        for (int i = 0; i < capacity; i++) {
+            keyArray[i] = INF;
+            position[i] = -1;
+        }
+    }
+    ~MinHeap() {    //Destructor
+        delete[] heapArray;
+        delete[] keyArray;
+        delete[] position;
+    }
+    void insert(int vertex, int key) {
+        if (size == capacity) {
+            return;
+        }
+        heapArray[size] = vertex;   //Inserting new vertex at end of heap
+        keyArray[vertex] = key;
+        position[vertex] = size;
+        size++;
+
+        int current = size-1;
+        int parent = (current-1)/2;
+        while (current != 0 && keyArray[heapArray[current]] < keyArray[heapArray[parent]]) {    //Fixing min heap
+            swap(heapArray[current], heapArray[parent]);
+            position[heapArray[current]] = current;
+            position[heapArray[parent]] = parent;
+            current = parent;
+            parent = (current-1)/2;
+        }
+    }
+    int extractMin() {
+        if (size == 0) {
+            return -1;
+        }
+        int min = heapArray[0];
+        int lastVertex = heapArray[size-1];
+        heapArray[0] = lastVertex;   //Removing min
+        position[lastVertex] = 0;    //Update position
+        position[min] = -1;
+        size--;
+        minHeapify(0);  //Fix entire heap after min is removed
+        return min;
+    }
+    void decreaseKey(int vertex, int newKey) {
+        int current = position[vertex];
+        int parent = (current-1)/2;
+        keyArray[vertex] = newKey;  //Update key
+        while (current != 0 && keyArray[heapArray[current]] < keyArray[heapArray[parent]]) {    //Fixing min heap
+            swap(heapArray[current], heapArray[parent]);
+            position[heapArray[current]] = current;
+            position[heapArray[parent]] = parent;
+            current = parent;
+            parent = (current-1)/2;
+        }
+    }
+    bool isInMinHeap(int vertex) {  //Checks if heapArray contains vertex
+        if (position[vertex] != -1) {
+            return true;
+        }
+        return false;
+    }
+    bool isEmpty() {    //Checks if the heap is empty
+        if (size == 0) {
+            return true;
+        }
+        return false;
+    }
 
 private:
     int* heapArray;        // Heap of vertex indices
@@ -17,8 +84,26 @@ private:
     int* position;         // Maps vertex to its position in heap
     int capacity;
     int size;
+    const int INF = 1000000000;
 
-    void minHeapify(int idx);
+    void minHeapify(int idx) {
+        int min = idx;  //Will find minimum vertex
+        int left = 2*idx + 1;
+        int right = 2*idx + 2;
+
+        if (left < size && keyArray[heapArray[left]] < keyArray[heapArray[min]]) {  //Check if left child is smaller than min
+            min = left;
+        }
+        if (right < size && keyArray[heapArray[right]] < keyArray[heapArray[min]]) {    //Check if right child is smaller than min
+            min = right;
+        }
+        if (min != idx) {   //Swaps min and idx if necessary
+            swap(heapArray[idx], heapArray[min]);
+            position[heapArray[idx]] = idx;
+            position[heapArray[min]] = min;
+            minHeapify(min);
+        }
+    }
 };
 
 #endif
